692f0ef275b8c0cffd2ae33077c29756787e8c1e
Bram Janssen <bram.janssen2@hva.nl>
2021-12-02 09:39:44 +0100

    Day Two

--- src/com/company/AOC2020/Day2.java
+++ src/com/company/AOC2020/Day2.java
@@ -10,11 +10,59 @@ public class Day2 extends PuzzleTemplate {
 
     @Override
     void puzzleOne(List<String> list) {
+        int horizontalPos = 0;
+        int depth = 0;
 
+        for (String e : list) {
+            e = e.replaceAll("\\s+", "");
+
+            if (e.contains("forward")) {
+                e = e.replaceAll("forward", "");
+                horizontalPos += Integer.parseInt(e);
+
+            }if (e.contains("down")) {
+                e = e.replaceAll("down", "");
+                depth += Integer.parseInt(e);
+
+            }if (e.contains("up")) {
+                e = e.replaceAll("up", "");
+                depth -= Integer.parseInt(e);
+
+            }
+        }
+            System.out.println("puzzleOne = " + horizontalPos * depth);
     }
 
     @Override
     void puzzleTwo(List<String> list) {
+        int horizontalPos = 0;
+        int depth = 0;
+        int aim = 0;
 
+        for (String e : list) {
+            e = e.replaceAll("\\s+", "");
+
+            if (e.contains("forward")) {
+                e = e.replaceAll("forward", "");
+                horizontalPos += Integer.parseInt(e);
+                if (aim > 0) depth += aim * Integer.parseInt(e);
+
+            }
+
+            if (e.contains("down")) {
+                e = e.replaceAll("down", "");
+                aim += Integer.parseInt(e);
+
+            }
+
+            if (e.contains("up")) {
+                e = e.replaceAll("up", "");
+                aim -= Integer.parseInt(e);
+
+            }
+
+        }
+
+        System.out.println("puzzleTwo = " + horizontalPos * depth);
     }
 }
--- src/com/company/AOC2020/Main.java
+++ src/com/company/AOC2020/Main.java
@@ -2,8 +2,8 @@ package com.company.AOC2020;
 
 public class Main {
     public static void main(String[] args) {
-        new Day1("1");
-//        new Day2("2");
+//        new Day1("1");
+        new Day2("2");
 
     }
 }
